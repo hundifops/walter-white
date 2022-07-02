3f5fc1c683b5d865843b79c707b0d17fd2934752
Hundi125f <hundi125f@proton.me>
2022-07-02 17:41:34 -0400

    just a fun thing that I did

---
+++ Say my name.py
@@ -0,0 +1,16 @@
+# Say my name
+
+text = input("Who the hell are you? ")
+print("Do what? I don't... I don't have a damn clue who the hell you are ")
+
+text = input()
+print("Bullshit. Cartel got Fring.")
+
+text = input()
+print("[Declan looks at Mike and Mike shakes his head]")
+
+text = input()
+print("[Quietly]... Heisenburg")
+
+text = input()
+print("You then get shot in the head by Heisenburg, the man who bombed a nursing home :-<")
